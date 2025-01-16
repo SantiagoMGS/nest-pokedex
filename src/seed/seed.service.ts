e861666717c88a44fd99a5c71883b19dbe5fec77
Santiago Martinez <smartinez9626@gmail.com>
2025-01-16 12:44:20 -0500

    add PokemonModule export and integrate it into SeedModule; update SeedService to use Mongoose for seeding Pokémon data

--- src/seed/seed.service.ts
+++ src/seed/seed.service.ts
@@ -1,22 +1,31 @@
 import { Injectable } from '@nestjs/common';
 import axios, { AxiosInstance } from 'axios';
 import { PokeResponse } from './interfaces/poke-response.interface';
+import { Model } from 'mongoose';
+import { Pokemon } from 'src/pokemon/entities/pokemon.entity';
+import { InjectModel } from '@nestjs/mongoose';
 
 @Injectable()
 export class SeedService {
   private readonly axios: AxiosInstance = axios;
+
+  constructor(
+    @InjectModel(Pokemon.name)
+    private readonly pokemonModel: Model<Pokemon>,
+  ) {}
 
   async executeSeed() {
     const { data } = await this.axios.get<PokeResponse>(
       'https://pokeapi.co/api/v2/pokemon?limit=251',
     );
 
-    data.results.forEach(({ name, url }) => {
+    data.results.forEach(async ({ name, url }) => {
       const segments = url.split('/');
       const no: number = +segments[segments.length - 2];
-      console.log({ name, no });
+
+      await this.pokemonModel.create({ name, no });
     });
 
-    return data.results;
+    return 'Seed executed';
   }
 }
