2277fcce393f83123399c20f1fed7b9f809fb030
Santiago Martinez <smartinez9626@gmail.com>
2025-01-16 11:29:41 -0500

    add CommonModule and integrate ParseMongoIdPipe for improved ID validation in PokemonController

--- src/pokemon/pokemon.service.ts
+++ src/pokemon/pokemon.service.ts
@@ -75,8 +75,10 @@ export class PokemonService {
   }
 
   async remove(id: string) {
-    const pokemon = await this.findOne(id);
-    await pokemon.deleteOne();
+    const { deletedCount } = await this.pokemonModel.deleteOne({ _id: id });
+    if (deletedCount === 0)
+      throw new BadRequestException(`Pokemon with id ${id} not found`);
+    return;
   }
 
   private handeExceptions(error: any) {
